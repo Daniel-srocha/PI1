50c937d7198de0c64ba9355b41e89bbf4a0bfb7d
mauapi2021 <mauapi161@gmail.com>
2023-05-10 21:37:35 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+        return render_template("homepage.html")
+
+@app.route("/vacinas_disponiveis")
+def vdisponiveis():
+        return render_template("vdisponiveis.html")
+
+@app.route("/quem_pode_se_vacinar")
+def qpsvacinar():
+        return render_template("quem_pode_se_vacinar.html")
+
+@app.route("/calendario")
+def calendario():
+        return render_template("calendario.html")
+
+@app.route("/locais")
+def locais():
+        return render_template("locais.html")
+
+if __name__ == "__main__":
+        app.run(debug=True)
