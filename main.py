a095c528ea3ff233ab80a719e5f283a514cbfded
Daniel Rocha <142742021+Daniel-srocha@users.noreply.github.com>
2023-10-21 17:31:26 -0300

    Update main.py
    refatorado main.py

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+# Configuração e extensões (se necessário)
+
+# Rotas
 @app.route("/")
 def homepage():
-        return render_template("homepage.html")
+    return render_template("homepage.html")
 
 @app.route("/vacinas_disponiveis")
 def vdisponiveis():
-        return render_template("vdisponiveis.html")
+    return render_template("vdisponiveis.html")
 
 @app.route("/quem_pode_se_vacinar")
 def qpsvacinar():
-        return render_template("quem_pode_se_vacinar.html")
+    return render_template("quem_pode_se_vacinar.html")
 
 @app.route("/calendario")
 def calendario():
-        return render_template("calendario.html")
+    return render_template("calendario.html")
 
 @app.route("/locais")
 def locais():
-        return render_template("locais.html")
+    return render_template("locais.html")
 
 if __name__ == "__main__":
-        app.run(debug=True)
+    app.run(debug=True)
